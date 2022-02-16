afe7342b7d671544a5eac17a70f33c38aba878d7
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2022-02-16 21:15:49 +0500

    add exceptional handling in controllers

--- controllers/author.js
+++ controllers/author.js
@@ -1,25 +1,65 @@
 const all = (req, res) => {
-  res.json({"message" : "All Author"});
+  try {
+    res.json({"message" : "All Author"});
+  } catch (error) {
+    res.status(error.status || 500).send({
+      "error": {
+        "message": error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 const get = (req, res) => {
-  const params = req.params;
-  res.json({"message" : "Author found", "data" : {"author" : {"id" : params.id}}});
+  try {
+    const params = req.params;
+    res.json({"message" : "Author found", "data" : {"author" : {"id" : params.id}}});
+  } catch (error) {
+    res.status(error.status || 500).send({
+      "error": {
+        "message": error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 const add = (req, res) => {
-  const params = req.params;
-  res.json({"message" : "Author added"});
+  try {
+    const params = req.params;
+    res.json({"message" : "Author added"});
+  } catch (error) {
+    res.status(error.status || 500).send({
+      "error": {
+        "message": error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 const update = (req, res) => {
-  const params = req.params;
-  res.json({"message" : "Author updated"});
+  try {
+    const params = req.params;
+    res.json({"message" : "Author updated"});
+  } catch (error) {
+    res.status(error.status || 500).send({
+      "error": {
+        "message": error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 const remove = (req, res) => {
-  const params = req.params;
-  res.json({"message" : "Author deleted"});
+  try {
+    const params = req.params;
+    res.json({"message" : "Author deleted"});
+  } catch (error) {
+    res.status(error.status || 500).send({
+      "error": {
+        "message": error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 module.exports = {
--- controllers/book.js
+++ controllers/book.js
@@ -1,25 +1,65 @@
 const all = (req, res) => {
-  res.json({"message" : "All Books"});
+  try {
+    res.json({"message" : "All Books"});
+  } catch (error) {
+    res.status(error.status || 500).send({
+      "error": {
+        "message": error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 const get = (req, res) => {
-  const params = req.params;
-  res.json({"message" : "Book found", "data" : {"book" : {"id" : params.id}}});
+  try {
+    const params = req.params;
+    res.json({"message" : "Book found", "data" : {"book" : {"id" : params.id}}});
+  } catch (error) {
+    res.status(error.status || 500).send({
+      "error": {
+        "message": error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 const add = (req, res) => {
-  const params = req.params;
-  res.json({"message" : "Book added"});
+  try {
+    const params = req.params;
+    res.json({"message" : "Book added"});
+  } catch (error) {
+    res.status(error.status || 500).send({
+      "error": {
+        "message": error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 const update = (req, res) => {
-  const params = req.params;
-  res.json({"message" : "Book updated"});
+  try {
+    const params = req.params;
+    res.json({"message" : "Book updated"});
+  } catch (error) {
+    res.status(error.status || 500).send({
+      "error": {
+        "message": error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 const remove = (req, res) => {
-  const params = req.params;
-  res.json({"message" : "Book deleted"});
+  try {
+    const params = req.params;
+    res.json({"message" : "Book deleted"});
+  } catch (error) {
+    res.status(error.status || 500).send({
+      "error": {
+        "message": error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 module.exports = {
