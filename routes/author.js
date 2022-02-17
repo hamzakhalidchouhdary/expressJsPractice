33a49ba92b8f163ab56496adf095a76a5e05144b
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2022-02-17 09:18:29 +0500

    modify route handlers

--- routes/author.js
+++ routes/author.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { author } = require('../controllers');
 
-router.get('/', (req, res, next) => author.all(req, res))
-router.get('/:id', (req, res, next) => author.get(req, res))
-router.post('/', (req, res, next) => author.add(req, res))
-router.put('/:id', (req, res, next) => author.update(req, res))
-router.delete('/:id', (req, res, next) => author.remove(req, res))
+router.get('/', author.all)
+router.get('/:id', author.get)
+router.post('/', author.add)
+router.put('/:id', author.update)
+router.delete('/:id', author.remove)
 
 module.exports = router;
--- routes/book.js
+++ routes/book.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const {book} = require('../controllers')
 
-router.get('/', (req, res) => book.all(req, res))
-router.post('/', (req, res) => book.add(req, res))
-router.get('/:id', (req, res) => book.get(req, res))
-router.put('/:id', (req, res) => book.update(req, res))
-router.delete('/:id', (req, res) => book.remove(req, res))
+router.get('/', book.all)
+router.post('/', book.add)
+router.get('/:id', book.get)
+router.put('/:id', book.update)
+router.delete('/:id', book.remove)
 
 module.exports = router;
