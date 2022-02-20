57fe2685a10fca979eed0709aac1d52424bbe1cc
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2022-02-20 12:22:03 +0500

    modify user auths

--- src/controllers/V1/userAuth.js
+++ src/controllers/V1/userAuth.js
@@ -1,9 +1,14 @@
 const { encodeUserToken } = require("../../utiliti/userAuthentication");
 const { sendErrorResponseV1: sendError } = require("../../utiliti/errorResponses");
 
+const validateUser = ({username, password}) => {
+  if (username === 'hamzakhalidchouhdary' && password === '123456789012345') return 
+  throw {message: 'invalid username or password', status: 401}
+}
 const login = (req, res) => {
   try {
     const userCredentials = req.body
+    validateUser(userCredentials);
     const token = encodeUserToken(userCredentials)
     res.status(200).json({
       "message" : "LOGIN SUCCESSFULLY....",
