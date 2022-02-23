a06b59adb564e33c0b6c5adf7be1a39150f31747
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2022-02-23 18:00:23 +0500

    move token authentication 1 level up

--- src/routes/V1/note.js
+++ src/routes/V1/note.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
-const { tokenAuthenticationV1 } = require('../../utiliti/tokenAuthentication');
 const {V1Controllers: V1} = require('../../controllers');
 
-router.get('/', tokenAuthenticationV1  , V1.note.all)
+router.get('/', V1.note.all)
 router.post('/', V1.note.add)
 router.get('/:id', V1.note.get)
 router.put('/:id', V1.note.update)
