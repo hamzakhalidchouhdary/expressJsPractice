134cffc7467669e6f03d2264f5e433e7230ec881
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2022-02-25 23:26:09 +0500

    add user registor schema

--- src/routes/V1/userAuth.js
+++ src/routes/V1/userAuth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const {V1Controllers: V1} = require('../../controllers')
 const { sendParamMissingError: throwError } = require('../../utiliti/errorResponses')
-const { loginValidationSchema } = require('../../utiliti/validationSchemas/authSchemas')
+const { loginValidationSchema, newUserSchema } = require('../../utiliti/validationSchemas/authSchemas')
 const { requestValidator } = require('../../utiliti/validationSchemas/requestValidator')
 
 router.post('/login', loginValidationSchema(), (req, res) => {
@@ -12,7 +12,14 @@ router.post('/login', loginValidationSchema(), (req, res) => {
     throwError(error, res);
   }
 })
-router.post('/join', V1.userAuth.join)
+router.post('/join', newUserSchema(), (req, res) => { 
+  try{
+    requestValidator(req);
+    V1.userAuth.join(req, res)
+  } catch (error) {
+    throwError(error, res)
+  }
+})
 router.post('/resetpassword', V1.userAuth.resetPassword)
 
 
