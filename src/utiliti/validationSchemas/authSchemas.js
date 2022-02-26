4b2fa46bf27900aa5493f70b3089f34407dd1573
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2022-02-26 18:57:10 +0500

    update registor schema and add changepassword schema

--- src/utiliti/validationSchemas/authSchemas.js
+++ src/utiliti/validationSchemas/authSchemas.js
@@ -1,4 +1,5 @@
 const { body } = require('express-validator');
+const passwordSize = 10;
 
 const loginValidationSchema = () => {
   return [
@@ -8,7 +9,7 @@ const loginValidationSchema = () => {
     body('password').
     exists().withMessage('required').
     notEmpty().withMessage('can not be empty').
-    isLength({min : 8}).withMessage('min length 8')
+    isLength({min : passwordSize}).withMessage(`min length ${passwordSize}`)
   ]
 }
 
@@ -18,15 +19,16 @@ const newUserSchema = () => {
     exists().withMessage('required').
     notEmpty().withMessage('cannot be null').
     custom(value => {
+      if (!(/^[a-zA-Z0-9_]+$/).test(value)) throw 'Invaild username'
       if (value === 'hamzakhalidchouhdary') throw 'username already in use';
       return true
     }),
     body('password').
     exists().withMessage('required').
     notEmpty().withMessage('cannot be null').
-    isLength({min : 8}).withMessage('min length 8').
+    isLength({min : passwordSize}).withMessage(`min length ${passwordSize}`).
     custom(value => {
-      const regEx = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$');
+      const regEx = new RegExp(`(^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])?)|((?=.*?[A-Z])(?=.*?[a-z]{1,})(?=.*?[0-9])?(?=.*?[#?!@$%^&*-]))).{${passwordSize},}$`);
       if (!regEx.test(value)) throw 'Week password'
       return true
     }),
@@ -38,9 +40,31 @@ const newUserSchema = () => {
       return true
     })
   ]
-} 
+}
+
+const changePasswordScheme = () => {
+  return [
+    body('password').
+    exists().withMessage('required').
+    notEmpty().withMessage('cannot be null').
+    isLength({min : passwordSize}).withMessage(`min length ${passwordSize}`).
+    custom(value => {
+      const regEx = new RegExp(`(^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])?)|((?=.*?[A-Z])(?=.*?[a-z]{1,})(?=.*?[0-9])?(?=.*?[#?!@$%^&*-]))).{${passwordSize},}$`);
+      if (!regEx.test(value)) throw 'Week password'
+      return true
+    }),
+    body('confirm_password').
+    exists().withMessage('requied').
+    notEmpty().withMessage('cannot be null').
+    custom((value, {req}) => {
+      if (value !== req.body.password) throw "Password does not match"
+      return true
+    })
+  ]
+}
 
 module.exports = {
   loginValidationSchema,
-  newUserSchema
+  newUserSchema,
+  changePasswordScheme
 }
