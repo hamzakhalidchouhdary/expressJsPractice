67f3d765a3609553476942bfb72764a0de0b2f65
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2022-02-28 21:49:00 +0500

    solve error hundler

--- src/utiliti/validationSchemas/noteItemSchema.js
+++ src/utiliti/validationSchemas/noteItemSchema.js
@@ -4,11 +4,7 @@ const noteItemSchema = () => {
   return [
     body('item').
     exists().withMessage('required').
-    notEmpty().withMessage('can not be null').
-    custom(vaule => {
-      // if ('/^[a-zA-Z0-9\s]+$/'.test(vaule)) return true
-      // throw 'invalid format'
-    })
+    notEmpty().withMessage('can not be null')
   ]
 }
 
